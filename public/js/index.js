1e3bc073b67270e89479a3f912a8d44be879cc06
Ashlesh Khajbage <ashleshuk@gmail.com>
2018-11-07 20:54:27 +0530

    index upadated

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,54 @@
 var socket = io();
 
-socket.on('connect',function(){
-  console.log('Connected to server');   
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-
-socket.on('disconnect',function(){
-  console.log('DisConnected to server');
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on('newMessage',function(message){
-    console.log('new email :',message);
-    var li=jQuery('<li></li>');
-    li.text(`${message.from}:${message.text}`);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
 
-    jQuery('#messages').append(li);
-  });
+  jQuery('#messages').append(li);
+});
 
-socket.emit('createMessage',{
-    from:'frank',
-    text:'hi'
-},function(data){
-  console.log('Got it',data);
-  
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
-e.preventDefault();
+  e.preventDefault();
 
-var messageTextbox = jQuery('[name=message]');
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
 
-socket.emit('createMessage', {
-    text: messageTextbox.val()
-}, function () {
-   // messageTextbox.val('')
+  });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
+  });
 });
